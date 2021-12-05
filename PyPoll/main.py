1773aa6bb0239cc0fd10fe22b70059d47e7c2267
Radford2688 <85588687+Radford2688@users.noreply.github.com>
2021-12-04 19:27:20 -0500

    Delete PyPoll directory

--- PyPoll/main.py
+++
@@ -1,9 +0,0 @@
-import os
-import csv
-csvpath = os.path.join ('Resources', 'budget_data.csv')
-with open (csvpath) as csvfile:
-    csv_reader = csv.reader (csv_file, delimiter =',')
-    print(csv_reader)    
-csv_reader = csv.DictReader(open('Resources/budget_data.csv'))    
-print(csv_reader)
-
